e69baaa3a03cf5cc70c0a2f0f44f7de9496aee83
José C. Pernías <jcpernias@gmail.com>
2021-07-15 17:07:49 +0200

    Add course specific rules

---
+++ course.mk
@@ -0,0 +1,39 @@
+# -*- mode: makefile; -*-
+
+subject_code := 1047
+units := \
+	Intro \
+	Size \
+	Welfare \
+	Market-Failure \
+	Efficiency-Equity \
+	Public-Goods \
+	Externalities \
+	Cost-Benefit
+
+unit_figs := \
+	Welfare \
+	Efficiency-Equity \
+	Public-Goods \
+	Externalities
+
+LANGUAGES := es
+DOC_TYPES := hdout pres
+
+docs_suffixes := $(addprefix _$(subject_code)-, $(LANGUAGES))
+docs_prefixes := $(foreach type,$(DOC_TYPES),$(addprefix $(type)-,$(units)))
+docs_base := $(foreach suffix,$(docs_suffixes),$(addsuffix $(suffix),$(docs_prefixes)))
+
+docs_pdf := $(addprefix $(outdir)/, $(addsuffix .pdf, $(docs_base)))
+
+## Automatic dependencies
+## ================================================================================
+docs_deps := $(addprefix $(depsdir)/, $(addsuffix .pdf.d, $(docs_base)))
+
+tex_deps_base := $(foreach suffix,$(docs_suffixes),$(addsuffix $(suffix),$(units)))
+tex_deps := $(addprefix $(depsdir)/unit-, $(addsuffix .tex.d, $(tex_deps_base)))
+
+unit_figs_deps := $(addprefix $(depsdir)/unit-,\
+	$(addsuffix _$(subject_code)-figs.d, $(unit_figs)))
+
+all_deps := $(docs_deps) $(tex_deps) $(unit_figs_deps)
