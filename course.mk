2d6224678b721cafcd212bbb459159446083084e
José C. Pernías <jcpernias@gmail.com>
2021-07-17 17:48:45 +0200

    Merge some handouts

--- course.mk
+++ course.mk
@@ -17,36 +17,33 @@ unit_figs := \
 	Public-Goods \
 	Externalities
 
+units := \
+	Intro \
+	Size \
+	Market-Failure \
+	Cost-Benefit
+
+unit_figs :=
 
 LANGUAGES := es
+DOC_TYPES := hdout pres
 
 
 docs_suffixes := $(addprefix _$(subject_code)-, $(LANGUAGES))
 docs_prefixes := $(addprefix pres-,$(units)) hdout-all
 docs_base := $(foreach suffix,$(docs_suffixes),$(addsuffix $(suffix),$(docs_prefixes)))
-$(info $(docs_base))
-
-# OLD CODE
-# ================================================================================
-ifdef FALSE
-LANGUAGES := es
-DOC_TYPES := hdout pres
-
-docs_suffixes := $(addprefix _$(subject_code)-, $(LANGUAGES))
-docs_prefixes := $(foreach type,$(DOC_TYPES),$(addprefix $(type)-,$(units)))
-docs_base := $(foreach suffix,$(docs_suffixes),$(addsuffix $(suffix),$(docs_prefixes)))
-
 docs_pdf := $(addprefix $(outdir)/, $(addsuffix .pdf, $(docs_base)))
 
 ## Automatic dependencies
 ## ================================================================================
 docs_deps := $(addprefix $(depsdir)/, $(addsuffix .pdf.d, $(docs_base)))
 
-tex_deps_base := $(foreach suffix,$(docs_suffixes),$(addsuffix $(suffix),$(units)))
+tex_deps_base := $(foreach suffix,$(docs_suffixes),\
+	$(addsuffix $(suffix),$(units) all))
+
 tex_deps := $(addprefix $(depsdir)/unit-, $(addsuffix .tex.d, $(tex_deps_base)))
 
 unit_figs_deps := $(addprefix $(depsdir)/unit-,\
 	$(addsuffix _$(subject_code)-figs.d, $(unit_figs)))
 
 all_deps := $(docs_deps) $(tex_deps) $(unit_figs_deps)
-endif
